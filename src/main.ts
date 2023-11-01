9f9ed5aa9070ea14e7d449afccbd4795a74e2fb0
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-11-01 20:38:26 +0900

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', c.json({
+  status: 'ok',
+  repo: 'https://github.com/nakasyou/nakasyou-api'
+}))
+
+const birthday = new Date('2009-11-03').getTime()
+
+const getOld = () => {
+  const now = new Date(new Date().toISOString().slice(0, 10)).getTime()
+  const ageMs = now - birthday
+  const ageInYears = ageMs / (365.25 * 24 * 60 * 60 * 1000)
+  const ageYears = Math.floor(ageInYears)
+  const ageMonths = Math.floor((ageInYears - ageYears) * 12)
+  const ageDays = Math.floor((ageInYears * 365.25) - (ageYears * 365.25) - (ageMonths * 30.4375))
+  return {
+    y: ageYears,
+    m: ageMonths,
+    d: ageDays
+  }
+}
+
+app.get('/profile', c.json({
+  name: 'Shotaro Nakamura',
+  gender: 'male',
+  post: '中2',
+  birthday: new Date(birthday),
+  old: getOld()
+}))
+
+Deno.serve(app.fetch)
